1a2c886fe85ccbec692b7222ced46767b44210bb
tanguydlz <59443450+tanguydlz@users.noreply.github.com>
2020-12-08 21:45:22 +0100

    final version

---
+++ package/clEval/R/constructor_multivariate_quantitative.R
@@ -0,0 +1,80 @@
+#' @title Multivarial quantitatives variable analysis to describe clusters
+#' @description This function realize multivarial analysis to describe clusters whith numerical variables
+#'
+#' @param active_variables a data frame. Must contains quantitatives variables
+#' @param clusters a numeric vector. Must contains clusters from a clustering
+#' @param quali.supp index of supplementary variables in the data frame. Muste be character
+#' @param show_graph logical. If FALSE, doesn't return any graph
+#' @param axes choice of dimensions for graph
+#'
+#' @return List of results (table for individuals and variables, correlation between dimension and clusters)
+#' @export
+#' @import FactoMineR ggplot2 factoextra RColorBrewer formattable
+
+multi.quanti<-function(active_variables, clusters,quali.supp=NULL,show_graph=NULL,axes=c(1,2)){
+  if (class(active_variables) !="data.frame"&class(active_variables)=="numeric"){
+    stop("Active_variables doesn't contain enough variables")
+  }else{
+    if (length(clusters)!=nrow(active_variables)){
+      stop("active_variables and y doesn't have the same length")
+    }
+  }
+  test<-active_variables[,-quali.supp]
+  if(all(sapply(test, is.numeric))==FALSE){
+    stop("Active variables (minus supplementary variables) aren't numeric")
+  }
+  test<-active_variables[,quali.supp]
+  if(all(sapply(test, is.character))==FALSE){
+    stop("supplementary aren't character")
+  }
+
+  # ACP
+  res.pca<-PCA(active_variables,quali.sup = quali.supp, scale.unit = TRUE, graph = FALSE)
+
+  #Visualisation of eigen values
+  eig.val <- get_eigenvalue(res.pca)
+  print(fviz_eig(res.pca, addlabels = TRUE, ylim = c(0, 50)))
+  print(eig.val)
+  #Choice of the number of axes
+  nb_dim<-readline(prompt="How many axes do you want to keep ? " )
+  nb_dim<-as.integer(nb_dim)
+
+  #Table for variables
+  var<-tab(get_pca_var(res.pca),nb_dim)
+
+  #Table for individus
+  ind <- tab(get_pca_ind(res.pca),nb_dim)
+
+  #Table of correlation between coorninates and clusters
+  correlation<-corr_coef(as.data.frame(res.pca$ind$coord),clusters)
+
+  #List of the results
+  instance <- list()
+  instance$eig.values<-eig.val
+  instance$var.tab <- var
+  instance$ind.tab<-ind
+  instance$correlation<-correlation
+  if(is.null(quali.supp)==FALSE){
+    instance$quali.supp<-res.pca$quali
+  }
+  class(instance) <- c("multi.quanti","list ")
+
+  #Show graphs
+  if(show_graph==TRUE){
+    if(!is.null(quali.supp)){
+      sup=TRUE
+      corr<-corr_coef(active_variables[,-quali.supp],clusters)$`Correlation coefficients table`
+      c<-rep(0,length(corr))
+      for(i in 1:length(corr)){c[i]<-corr[1,i]}
+    }else{
+      corr<-corr_coef(active_variables,clusters)$`Correlation coefficients table`
+      c<-rep(0,length(corr))
+      for(i in 1:length(corr)){c[i]<-corr[1,i]}
+    }
+
+    plot.multi.quanti(res.pca,as.factor(clusters),axes,sup,c)
+  }
+  #return the list of results
+  return(instance)
+
+}
---
+++ package/clEval/R/plot_univariate_qualitative.R
@@ -0,0 +1,102 @@
+#' @title Graphics for all the functions of qualitative variable
+#' @description This function allows to print any graphics for the variable univariate qualitative. We override the plot function.
+#'
+#' @param x a data frame. Contains all the data which needs to be print
+#' @param type a string. Choose the graphics to be print
+#' @param name a string. Name of the variable to be print
+#' @param digits a number. Number of digits for the values
+#' @param ... further arguments of print method
+#'
+#' @return graphics for the univariate qualitative function
+#' @export
+#' @import ggplot2 RColorBrewer fmsb
+#' @importFrom grDevices rgb
+
+plot.uni.quali<-function(x,type=NULL,name=NULL,digits=5,...){
+  if(type=="cramer"){
+    #Radarchart only if the number of variables is greater than 2
+    if(nrow(x) > 2){
+      data <- as.data.frame(matrix(as.numeric(x[,2]), ncol=nrow(x)))
+      print(radarchart(data, axistype=2, title = "Cramer's v by variable", pcol=rgb(0.2,0.5,0.5,0.9) , pfcol=rgb(0.2,0.5,0.5,0.5) , plwd=4 ,cglcol="blue", cglty=1, axislabcol="red", caxislabels=seq(0,20,5), cglwd=0.8,vlcex=0.8 ))
+
+      #Otherwise, we print a barplot
+    }else{
+      x = as.data.frame(x)
+      x[,2] = as.numeric(x[,2])
+      print(ggplot(data=x, aes(x=cramer_active_variables, y=cramer_val)) +
+              geom_bar(stat="identity", position=position_dodge(), width=0.75, fill=brewer.pal(n = 3, name = "Dark2")[1]) + ggtitle("Cramer's v by variables")+
+              scale_y_continuous(limits=c(0,1))+
+              geom_text(aes(label=cramer_val), position=position_dodge(width=0.9), vjust=-0.25, size=3)+
+              labs(x = "Variables", y = "Cramer's value")+
+              theme_minimal(base_size = 12) +
+              theme(axis.text.x = element_text(angle = -45, hjust=0, vjust=00),
+                    axis.title.y=element_text(size=rel(1.4)),
+                    axis.title.x=element_text(size=rel(1.4)),
+                    panel.background = element_rect(fill = NA, color = "gray40")))
+    }
+  }
+  if(type=="l.profil"){
+    title=paste("Proportion by modalities according to class for",name,sep=" ")
+    #Print a barplot for the contingency table
+    print(ggplot(data=as.data.frame(round(prop.table(x,1)*100,digits)), aes(x=active_variables, y=Freq)) +
+            geom_bar(stat="identity", position=position_dodge(),fill=brewer.pal(n = 3, name = "Dark2")[1]) + ggtitle(title) +
+            labs(x = "Modalities", y = "Proportion (row profils)")+
+            scale_y_continuous(expand=c(0.004,0)) +
+            theme_minimal(base_size = 12) +
+            theme(axis.text.x = element_text(angle = -45, hjust=0, vjust=00),
+                  axis.title.y=element_text(size=rel(1.4)),
+                  axis.title.x=element_text(size=rel(1.4)),
+                  panel.background = element_rect(fill = NA, color = "gray40")) +
+            facet_grid(clusters ~ ., labeller = labeller("clusters")) + facet_wrap(~ clusters, ncol=2))
+  }
+  if(type=="c.profil"){
+    title=paste("Proportion by modalities according to class for",name,sep=" ")
+    #Print a barplot for the contingency table
+    print(ggplot(data=as.data.frame(round(prop.table(x,2)*100,digits)), aes(x=active_variables, y=Freq)) +
+            geom_bar(stat="identity", position=position_dodge(),fill=brewer.pal(n = 3, name = "Dark2")[1]) + ggtitle(title) +
+            labs(x = "Modalities", y = "Proportion (col. profil)")+
+            scale_y_continuous(expand=c(0.004,0)) +
+            theme_minimal(base_size = 12) +
+            theme(axis.text.x = element_text(angle = -45, hjust=0, vjust=00),
+                  axis.title.y=element_text(size=rel(1.4)),
+                  axis.title.x=element_text(size=rel(1.4)),
+                  panel.background = element_rect(fill = NA, color = "gray40")) +
+            facet_grid(clusters ~ ., labeller = labeller("clusters")) + facet_wrap(~ clusters, ncol=2))
+  }
+  if(type=="h"){
+    title=paste("h value by modalities according to class for",name,sep=" ")
+    #We print our results in a barplot with threshold values
+    print(ggplot(data=x, aes(x=modality, y=h)) +
+            geom_bar(stat="identity", position=position_dodge(),fill=brewer.pal(n = 3, name = "Dark2")[1]) + ggtitle(title)+
+            geom_hline(aes(yintercept = 0.2,linetype = "small value"),colour = "yellow", size=1)+
+            geom_hline(aes(yintercept = 0.5,linetype = "medium value"),colour = "orange", size=1)+
+            geom_hline(aes(yintercept = 0.8,linetype = "large value"),colour = "darkred", size=1)+
+            scale_linetype_manual(name = "Thresholds", values=c(2,2,2),guide = guide_legend(override.aes = list(color = c("darkred", "orange","yellow"))))+
+            geom_text(aes(label=h), position=position_dodge(width=0.9), vjust=-0.25, size=3)+
+            labs(x = "Modalities", y = "h")+
+            theme_minimal(base_size = 12) +
+            theme(axis.text.x = element_text(angle = -45, hjust=0, vjust=00),
+                  axis.title.y=element_text(size=rel(1.4)),
+                  axis.title.x=element_text(size=rel(1.4)),
+                  panel.background = element_rect(fill = NA, color = "gray40"))+
+            facet_grid(clusters ~ ., labeller = labeller("clusters")) + facet_wrap(~ clusters, ncol=2))
+  }
+  if(type=="phi"){
+    title=paste("Phi value by modalities according to class for",name,sep=" ")
+    #We print our results in a barplot with threshold values
+    print(ggplot(data=x, aes(x=modality, y=phi)) +
+            geom_bar(stat="identity", position=position_dodge(),fill=brewer.pal(n = 3, name = "Dark2")[1]) + ggtitle(title)+
+            geom_hline(aes(yintercept = 0.1,linetype = "small value"),colour = "yellow", size=1)+
+            geom_hline(aes(yintercept = 0.3,linetype = "medium value"),colour = "orange", size=1)+
+            geom_hline(aes(yintercept = 0.5,linetype = "large value"),colour = "darkred", size=1)+
+            scale_linetype_manual(name = "Thresholds", values=c(2,2,2),guide = guide_legend(override.aes = list(color = c("darkred", "orange","yellow"))))+
+            geom_text(aes(label=phi), position=position_dodge(width=0.9), vjust=-0.25, size=3)+
+            labs(x = "Modalities", y = "phi")+
+            theme_minimal(base_size = 12) +
+            theme(axis.text.x = element_text(angle = -45, hjust=0, vjust=00),
+                  axis.title.y=element_text(size=rel(1.4)),
+                  axis.title.x=element_text(size=rel(1.4)),
+                  panel.background = element_rect(fill = NA, color = "gray40"))+
+            facet_grid(clusters ~ ., labeller = labeller("clusters")) + facet_wrap(~ clusters, ncol=2))
+  }
+}
